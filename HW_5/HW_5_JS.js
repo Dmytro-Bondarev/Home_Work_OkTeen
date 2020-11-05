096f7d3a28f5be516c73b9cea91d147653775ce0
Dmytro-Bondarev <dima.bondarev1209@gmail.com>
2020-11-05 15:21:34 +0200

    HW_5

---
+++ HW_5/HW_5_JS.js
@@ -0,0 +1,164 @@
+// присвоить каждому из следующих значений свою переменную: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false Вывести каждую при помощи console.log , alert, document.write
+
+//let text1 = 'hello'
+//let text2 = 'owu'
+//let text3 = 'com'
+//let text4 = 'ua'
+//let numeral1 = 1
+//let numeral2 = 10
+//let numeral3 = -999
+//let numeral4 = 123
+//let numeral5 = 3.14
+//let numeral6 = 2.7
+//let numeral7 = 16
+//let bullying1 = true
+//let bullying2 = false
+
+//console.log (text1+' '+text2+' '+text3+' '+text4+' '+numeral1+' '+numeral2+' '+numeral3+' '+numeral4+' '+numeral5+' '+numeral6+' '+numeral7)
+
+// переопределить каждую переменную из задания 1 дав им произвольные значения Вывести каждую при помощи console.log , alert, document.write
+//let hi = 'hello'
+//let shool = 'owu'
+//let domen1 = 'com'
+//let domen2 = 'ua'
+//let number1 = 1
+//let number2 = 10
+//let negative = -999
+//let enumeration = 123
+//let fraction1 = 3.14
+//let fraction2 = 2.7
+//let fraction3 = 16
+//let like = true
+//let bad = false
+
+//console.log (hi+' '+shool+' '+domen1+' '+domen2+' '+number1+' '+number2+' '+negative+' '+enumeration+' '+fraction1+' '+fraction2+' '+fraction3+' '+like+' '+bad)
+
+//alert (hi+' '+shool+' '+domen1+' '+domen2+' '+number1+' '+number2+' '+negative+' '+enumeration+' '+fraction1+' '+fraction2+' '+fraction3+' '+like+' '+bad)
+
+//document.write (hi+' '+shool+' '+domen1+' '+domen2+' '+number1+' '+number2+' '+negative+' '+enumeration+' '+fraction1+' '+fraction2+' '+fraction3+' '+like+' '+bad)
+
+// - Создать 3 числовых и 3 стринговых константы. Вывести каждую при помощи console.log , alert, document.write
+
+//const digit1 = 25
+//const digit2 = 66
+//const digit3 = 43
+//const screening1 ='Welcome'
+//const screening2 ='Web'
+//const screening3 ='Internet'
+
+//console.log (digit1+' '+digit2+' '+digit3 )
+//alert (digit1+' '+digit2+' '+digit3 )
+//document.write (digit1+' '+digit2+' '+digit3 )
+
+//console.log (screening1+' '+screening2+' '+screening3)
+//alert (screening1+' '+screening2+' '+screening3)
+//document.write (screening1+' '+screening2+' '+screening3)
+// - при помощи 3х разных prompt() получить 3 слова которые являются вашими ФИО. Для фамилии имени и отчества создать разные переменные. Вывести каждую при помощи console.log , alert, document.write
+//let name = 'Enter name'
+//let surname = 'Enter surname '
+//let patronymic = 'Enter patronymic'
+
+//prompt(name)
+//prompt(surname)
+//prompt(patronymic)
+
+//console.log (name+' '+surname+' '+patronymic)
+//alert (name+' '+surname+' '+patronymic)
+//document.write (name+' '+surname+' '+patronymic)
+
+// Взять переменные из задания 4 и сконкатенировать их в одной переменной person.
+
+//let person = (name+surname+patronymic);
+
+// Взять задние 4 и 5 и применить его к ФИО всех членов своей семьи.
+
+//let name_mother = 'Enter name mother'
+//let surname_mother = 'Enter surname mother'
+//let patronymic_mother = 'Enter patronymic mother'
+
+//let name_father = 'Enter name father'
+//let surname_father = 'Enter surname father'
+//let patronymic_father = 'Enter patronymic father'
+
+//prompt(name_mother+' '+surname_mother+' '+patronymic_mother)
+//prompt(name_father+' '+surname_father+' '+patronymic_father)
+//console.log (name_father+' '+surname_father+' '+patronymic_father)
+//alert (name_father+' '+surname_father+' '+patronymic_father)
+//document.write (name_father+' '+surname_father+' '+patronymic_father)
+
+//console.log (name_mother+' '+surname_mother+' '+patronymic_mother)
+//alert (name_mother+' '+surname_mother+' '+patronymic_mother)
+//document.write (name_mother+' '+surname_mother+' '+patronymic_mother)
+
+// при помощи prompt() получить 3 числа. Привести их к числовому типу при помощи +. вывести их в консоль.
+//let nom_1 = +prompt('nom_1')
+//let nom_2 = +prompt('nom_2')
+//let nom_3 = +prompt('nom_3')
+
+//console.log(nom_1,nom_2,nom_3)
+
+//при помощи prompt() получить 4 числа. Привести их к числовому типу при помощи parseInt. Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+//let nom_1 = prompt('nom_1')
+//let nom_2 = prompt('nom_2')
+//let nom_3 = prompt('nom_3')
+//let nom_4 = prompt('nom_4')
+
+//let parseInt = parseInt(nom_1,nom_2,nom_3,nom_4) 
+//console.log (parseInt)
+
+//при помощи prompt()  получить 3 числа с плавающей точекой. при помощи parseFloat привести их к соответсвующему типу. Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+// let nom_1 = prompt('веди число');
+// let nom_2 = prompt('веди число');
+// let nom_3 = prompt('веди число');
+//console.log(parseFloat(nom_1)+parseFloat(nom_2)+parseFloat(nom_3));
+//при помощи prompt()  получить 3 числа с плавающей точекой. Округлить их при помощи Math.round Сложить их между собой записав результат в переменную  и вывести в консоль браузера
+// let nom_1 = prompt('веди число');
+// let nom_2 = prompt('веди число');
+// let nom_3 = prompt('веди число');
+// console.log(Math.round(nom_1)+Math.round(nom_2)+Math.round(nom_3));
+//при помощи prompt()  получить 2 целых числа. Привести их к целочисленному типу. Первое число - это число которое будут возводить в степень. Второе число - это число которое является степенью. При помощи Math.pow возвести первое число в степень второго числа.
+// let num = prompt()
+// let pow = prompt('pow')
+// let result = Math.pow(num,pow)
+// console.log (result)
+// При помощи оператора опредеоения типа typeof определить типы следующих переменных и вывести их в консоль let a = 100; let b = '100'; let c = true; let d = undefined;
+// let a = 100;
+// let b = '100';
+// let c = true; 
+// let d = undefined;
+// console.log(typeof a,typeof b,typeof  c,typeof d);
+
+// Поставьте соответствующий оператор в выражениях что бы получился соответсвующий результат.
+// В одинаковых выражениях в задании не использовать два одинаковых оператора.
+// Каждое выражение вложить в свою переменную
+//     5 < 6 -> true
+//     5 > 6 -> false
+//     5 == 6 -> false
+//     5 >= 6 -> false
+//     10 == 10 -> true
+//     10 <= 10 -> true
+//     10 < 10 -> false
+//     10 > 10 -> false
+//     10 != 10 -> false
+//     123 === '123' -> false
+//     123 == '123' -> true
+
+// Дополнительно:
+// - Посмотрев на следюующие выражения, скажите, каков будет вывод в консоль
+console.log(132 > 100 && 45 < 12 ); 'folse'
+console.log(34 > 33 && 23 < 90 ); true
+console.log(99 > 100 && 45 > 12 ); folse
+console.log(132 > 100 || 45 < 12 ); true
+console.log(111 > 11 || 45 < 111 ); true
+console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) ); true
+console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) );  true
+console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) ); false
+console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 )); true
+console.log(!!'-1'); true
+console.log(!!-1); true
+console.log(!!'0'); true
+console.log(!!'null'); true
+console.log(!!'undefined'); true
+console.log(!!(3/'owu')); false
+console.log((111 > 11 || 45 < 111) ||  !!'0'); true
+console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 )); folse
